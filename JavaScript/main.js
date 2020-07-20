389312a188102a856fdda5fd11e099366091384b
TommyChen <thomasyutao2004@gmail.com>
2020-07-20 12:17:56 +1000

    Blacksquad

---
+++ JavaScript/main.js
@@ -0,0 +1,28 @@
+function adjustScrollPosition() {
+  let scroll = 0;
+  window.onscroll = () => {
+    let nowScroll = document.documentElement.scrollTop;
+    if (nowScroll > 0 && scroll <= nowScroll) {
+      scroll = nowScroll;
+      document.getElementById("below1024px").style.position = "absolute";
+      now.top = "0px";
+    } else {
+      scroll = nowScroll;
+      document.getElementById("below1024px").style.position = "fixed";
+      now.top = "100px";
+    }
+  };
+}
+adjustScrollPosition();
+
+function dropdown() {
+  let now = document.getElementById("dropdownBar").style;
+  if (now.display == "flex") {
+    toNone = () => (now.display = "none");
+    now.animationName = "disappear2";
+    setTimeout(toNone, 300);
+  } else {
+    now.display = "flex";
+    now.animationName = "disappear";
+  }
+}
